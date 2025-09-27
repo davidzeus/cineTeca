911d39e5ff67b15d618e8609378dfac7922e0732
Cristian Gonzalez <thehack.dg@gmail.com>
2025-09-27 18:14:47 -0300

    funcionando en compilacion de linux

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.cineteca"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.cineteca"
         minSdk = 34
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
